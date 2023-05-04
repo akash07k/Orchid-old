5c2a16b9449629ca7179cba12e0ce9e05f2ec9d7
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-05-05 00:54:10 +0530

    feat(orchid): add initial OrchidApp class inheriting from Application class

---
+++ Orchid.App/OrchidApp.cs
@@ -0,0 +1,31 @@
+﻿using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchid.App
+{
+    [Application(Label = "@string/app_name")]
+    public class OrchidApp : Application
+    {
+        #region Public Constructors
+
+        public OrchidApp(IntPtr handle, JniHandleOwnership ownership)
+    : base(handle, ownership)
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+        }
+
+        #endregion Public Methods
+    }
+}
