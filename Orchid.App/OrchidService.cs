dc1403c6f57129286b07c7fd78b7ad1ba2f45475
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-05-04 21:39:02 +0530

    feat(service): add initial accessibility service

---
+++ Orchid.App/OrchidService.cs
@@ -0,0 +1,39 @@
+﻿using Android.AccessibilityServices;
+using Android.Runtime;
+using Android.Views.Accessibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchid.App
+{
+    [Service(Name = "Services.OrchidService", Label = "@string/app_name", Enabled = true, Exported = false, Permission = "android.permission.BIND_ACCESSIBILITY_SERVICE"), IntentFilter(new[] { "android.accessibilityservice.AccessibilityService" }), MetaData("android.accessibilityservice", Resource = "@xml/serviceconfig")]
+    [Register("com.techromantica.orchid.service")]
+    public class OrchidService : AccessibilityService
+    {
+        #region Public Methods
+
+        public override void OnAccessibilityEvent(AccessibilityEvent? e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnInterrupt()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion Public Methods
+
+        #region Protected Methods
+
+        protected override void OnServiceConnected()
+        {
+            base.OnServiceConnected();
+        }
+
+        #endregion Protected Methods
+    }
+}
