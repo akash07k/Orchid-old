d90ee8d2c31bfb552379d43b267789a1e0ebb8b9
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-05-06 22:29:32 +0530

    feat: implement the TTS utility class

---
+++ Orchid.App/Utils/TTS.cs
@@ -0,0 +1,62 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Speech.Tts;
+using Android.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchid.App.Utils
+{
+    public class TTS : Java.Lang.Object, TextToSpeech.IOnInitListener
+    {
+        #region Private Fields
+
+        private const string TAG = "Orchid.TTS";
+        private readonly Context _context;
+        private readonly TextToSpeech _textToSpeech;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        public TTS(Context context)
+        {
+            Log.Info(TAG, "Initializing the TTS");
+            _context = context;
+            _textToSpeech = new(context, this);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public void OnInit([GeneratedEnum] OperationResult status)
+        {
+            if (status == OperationResult.Success)
+            {
+                Log.Info(TAG, "TTS initialized successfully");
+            }
+            if (status == OperationResult.Error)
+            {
+                Log.Error(TAG, "Error while initializing the TTS");
+            }
+        }
+
+        public void Shutdown()
+        {
+            Log.Info(TAG, "Shutting down the TTS");
+            _textToSpeech.Shutdown();
+        }
+
+        public void Speak(string text)
+        {
+            Log.Info(TAG, $"Speaking the text: {text}");
+            _textToSpeech.Speak(text, QueueMode.Flush, null, null);
+        }
+
+        #endregion Public Methods
+    }
+}
