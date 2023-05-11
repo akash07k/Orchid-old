bb4ac7bac96ee6635174b4de3df896fd2733eb2a
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-05-12 01:29:47 +0530

    feat(interfaces): add IEventProcessor interface
    so that we can have a strict contract for all the event processors

---
+++ Orchid.App/Interfaces/IEventProcessor.cs
@@ -0,0 +1,18 @@
+﻿using Android.Views.Accessibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchid.App.Interfaces
+{
+    public interface IEventProcessor
+    {
+        #region Public Methods
+
+        void OnEvent(AccessibilityEvent accessibilityEvent);
+
+        #endregion Public Methods
+    }
+}
