32a4b28685cffb006ba7b4306aed8408b7390983
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-05-12 01:31:11 +0530

    feat(SpeechEventProcessor): implement IEventProcessor interface

--- Orchid.App/EventProcessors/SpeechEventProcessor.cs
+++ Orchid.App/EventProcessors/SpeechEventProcessor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Android.Views.Accessibility;
+using Orchid.App.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace Orchid.App.EventProcessors
 {
-    public class SpeechEventProcessor
+    public class SpeechEventProcessor : IEventProcessor
     {
+        #region Public Methods
+
+        public void OnEvent(AccessibilityEvent accessibilityEvent)
+        {
+            // TODO
+        }
+
+        #endregion Public Methods
     }
 }
