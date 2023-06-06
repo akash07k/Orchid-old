4d1484c3955f7ee4d3d5ce4f345140c61c76b0f2
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-06-07 01:52:43 +0530

    feat(processor): add and implement initial FocusEventProcessor

---
+++ Orchid.App/EventProcessors/FocusEventProcessor.cs
@@ -0,0 +1,53 @@
+﻿using Android.Content;
+using Android.Util;
+using Android.Views.Accessibility;
+using Orchid.App.Interfaces;
+using Orchid.App.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orchid.App.EventProcessors
+{
+    public class FocusEventProcessor : IEventProcessor
+    {
+        #region Private Fields
+
+        private const string _TAG = "Orchid.FocusEventProcessor";
+        private Context _context;
+        private TTS _tts;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        public string Name
+        {
+            get { return _TAG; }
+        }
+
+        #endregion Public Properties
+
+        #region Public Constructors
+
+        public FocusEventProcessor(Context context)
+        {
+            Log.Debug(_TAG, $"Initializing the {_TAG}.");
+            _context = context;
+            _tts = new TTS(context);
+        }
+
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public void OnEvent(AccessibilityEvent accessibilityEvent)
+        {
+            Log.Debug(_TAG, "Received the event, processing it.");
+        }
+
+        #endregion Public Methods
+    }
+}
