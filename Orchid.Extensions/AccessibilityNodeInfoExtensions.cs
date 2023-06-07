c365d065212750106b079178aa621fc2c5b6f652
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-06-08 00:25:42 +0530

    feat(ANIE): add LogNode extension method for logging the node content

--- Orchid.Extensions/AccessibilityNodeInfoExtensions.cs
+++ Orchid.Extensions/AccessibilityNodeInfoExtensions.cs
@@ -58,6 +58,67 @@ namespace Orchid.Extensions
             return hasText;
         }
 
+        /// <summary>
+        /// Logs the information of a <see cref="NodeInfo"/> object.
+        /// </summary>
+        /// <param name="node">The <see cref="NodeInfo"/> object to log.</param>
+        /// <returns>A string containing the logged information.</returns>
+        /// <remarks>
+        /// This method logs various properties and states of the <paramref name="node"/> object, including its content, focus states, actionable states, other states, and hierarchy information.
+        /// </remarks>
+        public static string LogNode(this NodeInfo node)
+        {
+            List<string> logList = new List<string>();
+            try
+            {
+                // Content.
+                logList.Add("\n## Content:\n");
+                logList.Add($"Content description: {node?.ContentDescription}");
+                logList.Add($"Hint text: {node?.HintText}");
+                logList.Add($"Text: {node?.Text}");
+                logList.Add($"Package name: {node?.PackageName}");
+                logList.Add($"Id: {node?.ViewIdResourceName}");
+                logList.Add($"Unique Id: {node?.UniqueId}");
+                logList.Add($"Class name: {node?.ClassName}");
+                logList.Add($"Is heading: {node?.Heading}");
+                logList.Add($"Is important for accessibility: {node?.ImportantForAccessibility}");
+                logList.Add($"Error: {node?.Error}");
+
+                // Focus states.
+                logList.Add("\n## Focus states");
+                logList.Add($"Is focusable: {node?.Focusable}");
+                logList.Add($"Is focused: {node?.Focused}");
+                logList.Add($"Is accessibility focused: {node?.AccessibilityFocused}");
+                logList.Add($"Is screen reader focusable: {node?.ScreenReaderFocusable}");
+
+                // Actionable states.
+                logList.Add($"\n## Actionable states");
+                logList.Add($"Is checkable: {node?.Checkable}");
+                logList.Add($"Is clickable: {node?.Clickable}");
+                logList.Add($"Is long clickable: {node?.LongClickable}");
+                logList.Add($"Is editable: {node?.Editable}");
+                logList.Add($"Is checked: {node?.Checked}");
+                logList.Add($"Is enabled: {node?.Enabled}");
+
+                // Other states.
+                logList.Add($"\n## Other states");
+                logList.Add($"Is password: {node?.Password}");
+
+                // Hierarchy
+                logList.Add("\n## Hierarchy");
+                logList.Add($"Parent class name: {node?.Parent?.ClassName}");
+                logList.Add($"Child count: {node?.ChildCount}");
+                logList.Add($"Pane title: {node?.PaneTitle}");
+            }
+            catch (Java.Lang.NullPointerException ex)
+            {
+                Log.Error(_TAG, ex.Message);
+            }
+            string logContent = logList?.JoinToString("\n");
+            Log.Debug(_TAG, $"{logContent}");
+            return logContent;
+        }
+
         #endregion Public Methods
     }
 }
