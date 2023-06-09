2f34efe63eac872f97860de8da0ffcc91a241369
Akash Kakkar <6381747+akash07k@users.noreply.github.com>
2023-06-10 01:07:28 +0530

    docs(TTS): add xml doc comments

--- Orchid.App/Utils/TTS.cs
+++ Orchid.App/Utils/TTS.cs
@@ -8,8 +8,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Provides text-to-speech functionality for the application.
+/// </summary>
 namespace Orchid.App.Utils
 {
+    /// <summary>
+    /// Represents a class that handles text-to-speech operations.
+    /// </summary>
     public class TTS : Java.Lang.Object, TextToSpeech.IOnInitListener
     {
         #region Private Fields
@@ -22,6 +28,10 @@ namespace Orchid.App.Utils
 
         #region Public Constructors
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TTS"/> class with the specified context.
+        /// </summary>
+        /// <param name="context">The context used for text-to-speech operations.</param>
         public TTS(Context context)
         {
             Log.Info(_TAG, "Initializing the TTS");
@@ -33,6 +43,10 @@ namespace Orchid.App.Utils
 
         #region Public Methods
 
+        /// <summary>
+        /// Called to indicate the completion of the TextToSpeech engine initialization.
+        /// </summary>
+        /// <param name="status">The initialization status.</param>
         public void OnInit([GeneratedEnum] OperationResult status)
         {
             if (status == OperationResult.Success)
@@ -45,12 +59,19 @@ namespace Orchid.App.Utils
             }
         }
 
+        /// <summary>
+        /// Shuts down the TextToSpeech engine.
+        /// </summary>
         public void Shutdown()
         {
             Log.Info(_TAG, "Shutting down the TTS");
             _textToSpeech.Shutdown();
         }
 
+        /// <summary>
+        /// Speaks the specified text using the TextToSpeech engine.
+        /// </summary>
+        /// <param name="text">The text to be spoken.</param>
         public void Speak(string text)
         {
             Log.Debug(_TAG, $"Speaking the text: {text}.");
